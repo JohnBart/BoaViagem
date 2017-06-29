23c3def20e6741a678e3951983c02b3d46b37958
João Paulo Bart Dias <joaopaulobart@hotmail.com>
2017-06-29 00:52:13 -0300

    viagens

--- app/src/main/java/com/example/boaviagem/DashboardActivity.java
+++ app/src/main/java/com/example/boaviagem/DashboardActivity.java
@@ -1,10 +1,11 @@
 package com.example.boaviagem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+/*import android.widget.TextView;
+import android.widget.Toast;*/
 
 /**
  * Created by João Paulo Bart Dias on 28/06/2017.
@@ -18,9 +19,20 @@ public class DashboardActivity extends Activity {
         setContentView(R.layout.dashboard);
     }
 
-    public void selecionarOpcao(View view){
+    /*public void selecionarOpcao(View view){
         TextView textView = (TextView) view;
         String opcao = "Opção: "+textView.getText().toString();
         Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+    */
+
+    public class ViagemActivity extends Activity {
+
+        public void selecionarOpcao(View view){
+            Switch(view.getId()){
+                case R.id.nova_viagem:
+                    startActivity(new Intent((this, viagemActivity.class)));
+                    break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/boaviagem/ViagemActivity.java
@@ -0,0 +1,12 @@
+package com.example.boaviagem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Switch;
+
+/**
+ * Created by João Paulo Bart Dias on 29/06/2017.
+ */
+
+
